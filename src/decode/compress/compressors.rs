854ec4db4a793123b5acfc62739306e263605900
Hannes Furmans <hannes.furmans@web.de>
2021-01-19 11:05:17 +0100

    Switch to zstd instead of deflate; Deflate Modules are still available though

--- src/decode/compress/compressors.rs
+++ src/decode/compress/compressors.rs
@@ -1,11 +1,21 @@
 use bytes::{Bytes, Buf};
 use libflate::deflate::Decoder;
 use std::io::Read;
-
+use zstd::decode_all;
+/*
 pub fn deflate_dec(inp: &Bytes) -> Bytes {
     let data = inp.to_owned().reader();
     let mut decoder = Decoder::new(data);
     let mut decoded_data = Vec::new();
     decoder.read_to_end(&mut decoded_data).unwrap();
+    Bytes::from(decoded_data)
+}
+*/
+
+pub fn deflate_dec(inp: &Bytes) -> Bytes {
+    let data = inp.to_owned().reader();
+    // let mut decoder = Decoder::new(data);
+    let mut decoded_data = decode_all(data).unwrap();
+    // decoder.read_to_end(&mut decoded_data).unwrap();
     Bytes::from(decoded_data)
 }
--- src/encode/compress/compressors.rs
+++ src/encode/compress/compressors.rs
@@ -1,12 +1,25 @@
 use bytes::{Bytes, Buf};
-
 use std::io;
 use crate::encode::flatten::lists::bytes_list;
+use zstd::stream::Encoder;
+use zstd::encode_all;
+use std::io::Write;
 
+/*
 pub fn deflate(inp: &Bytes) -> Bytes {
     let mut data = inp.to_owned().reader();
     let mut encoder = libflate::deflate::Encoder::new(Vec::new());
     io::copy(&mut data, &mut encoder).unwrap();
     let encoded_data = encoder.finish().into_result().unwrap();
+    bytes_list(&encoded_data)
+}
+*/
+
+pub fn deflate(inp: &Bytes) -> Bytes {
+    let mut data= inp.to_vec();
+    let mut encoder = Encoder::new(Vec::new(), 21).unwrap();
+    encoder.multithread(num_cpus::get() as u32);
+    encoder.write_all(&data).unwrap();
+    let encoded_data = encoder.finish().unwrap();
     bytes_list(&encoded_data)
 }
