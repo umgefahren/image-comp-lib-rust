efe990eeb2160e35e799239c71fe3376ab2394d2
Hannes <hannes.furmans@web.de>
2021-04-07 16:40:43 +0200

    Removed debug notice

--- src/encode/clustering/clustering_methods.rs
+++ src/encode/clustering/clustering_methods.rs
@@ -9,9 +9,6 @@ pub fn kmeans_clustering(cloud: &[[f64; 5]], k: usize) -> Vec<Vec<usize>> {
     }
     let kmeans = KMeans::new(inp, cloud.len(), 5);
     let result = kmeans.kmeans_lloyd(k, 10, KMeans::init_kmeanplusplus, &KMeansConfig::default());
-    println!("Centroids => {:?}", result.centroids);
-    println!("Points => {}", cloud.len());
-    println!("Cluster-Assingments => {:?}", result.assignments.len());
     let mut assingnments = vec![];
     for idx in 0..result.centroids.len() {
         let assigns: Vec<usize> = result.assignments.iter().enumerate()
